79daecdc5d7f34f0099c644ced8390210a5d054a
RudArtem <poseydon20001201@gmail.com>
2020-10-22 01:07:01 +0300

    AT-1, AT-1 modified

--- src/test/java/task12/Task.java
+++ src/test/java/task12/Task.java
@@ -1,13 +1,14 @@
 package task12;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Task {
     static void task1(){
-        Random rand = new Random();
+        Random random = new Random(); // fuller name of variable
         int[] mass = new int[10];
         for (int i = 0; i <mass.length;i++){
-            mass[i] = rand.nextInt();
+                mass[i] = random.nextInt(); // fuller name of variable
             if (mass[i]<0) mass[i]= 0;
             System.out.println(mass[i]);
         }
@@ -15,19 +16,25 @@ public class Task {
     static  void task2(){
         int sumOfWords = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number of lines: ");
-        int numberOfLines = sc.nextInt();
-        String mass[] = new String[numberOfLines];
-        for (int i = 0; i < numberOfLines ; i++) {
-            System.out.println("enter values of these lines");
-            String value = sc.next();
-            mass[i] = value;
-            sumOfWords += mass[i].length();
+        try{ //when user doesn`t enter numbers , user get message
+            System.out.println("enter the number of lines: ");
+            int numberOfLines = sc.nextInt();
+            String mass[] = new String[numberOfLines];
+            for (int i = 0; i < numberOfLines ; i++) {
+                System.out.println("enter values of these lines");
+                String value = sc.next();
+                mass[i] = value;
+                sumOfWords += mass[i].length();
+            }
+            double averageLengh = sumOfWords/numberOfLines;
+            System.out.println("Your average length is: " + averageLengh);
         }
-        double averageLengh = sumOfWords/numberOfLines;
-        System.out.println("Your average length is: " + averageLengh);
+        catch (InputMismatchException ex){
+            System.out.println("/////Введено не число//////");
+            System.out.println("Введите заново");
+            task2();
+        }
     }
-
     public static void main(String[] args){
         task1();
         task2();
